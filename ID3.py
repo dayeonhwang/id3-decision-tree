d7bade940e686b389a9deedab6bb8419941a7205
dayeonhwang <dahwang2018@u.northwestern.edu>
2017-04-12 18:23:57 -0500

    changed ID3 function and added a test file

--- ID3.py
+++ ID3.py
@@ -5,10 +5,12 @@ from collections import Counter
 from collections import defaultdict
 
 #global variable
-attributes = [key for key in examples.keys()] # a list of attributes
+attributes = [] # a list of attributes
+flag = 0
 
 def ID3(examples, default):
   global attributes
+  global flag
   '''
   Takes in an array of examples, and returns a tree (an instance of Node) 
   trained on the examples.  Each example is a dictionary of attribute:value pairs,
@@ -19,8 +21,12 @@ def ID3(examples, default):
   fill_missing_attr(examples)
   tree = Node()
   tree.mode = mode(examples)
-  same_class_value = check_same_class(examples)
-  best_attr = choose_best_attr(examples, attributes)
+  
+  if flag == 0:
+    attributes = [key for key in examples[0]]
+    if 'Class' in attributes:
+      attributes.remove('Class')
+    flag = 1
 
   # corner cases
   # 1. example set is empty
@@ -28,26 +34,35 @@ def ID3(examples, default):
     tree.label = default
 
   # 2. all examples have same class value -> tree is a leaf node
-  else if check_same_class(examples):
+  elif check_same_class(examples):
     tree.label = check_same_class(examples)
 
   # 3. non-trivial split of examples is possible (all examples have same attribute values) -> mode class value
-  else if len(attributes)==0:
+  elif len(attributes) == 0:
     tree.label = mode(examples)
-
   # general case:
   else:
     best_attr = choose_best_attr(examples, attributes)
-    tree.split_attr = best_attr
-    tree.split_attr_index = attributes.index(best_attr)
-    tree.children = {}
-    sub_examples = split_examples(examples, best_attr) #{val1: {sub_examples_1}, val2: {sub_examples_2}, ...}
+    if (best_attr == 'Class'):
+      best_attr = None
+    print 'chose best attr = ' + str(best_attr)
+    #check if best_attr of all examples are identical
+    if (best_attr == None):
+      tree.label = mode(examples)
+    elif check_same_value(examples, best_attr) == True:
+      tree.label = mode(examples)
+    #otherwise, continue
+    else:
+      tree.split_attr = best_attr
+      tree.split_attr_index = attributes.index(best_attr)
+      children = {}
+      sub_examples = split_examples(examples, best_attr) #{val1: {sub_examples_1}, val2: {sub_examples_2}, ...}
 
-    for val, sub_e in sub_examples.iteritems():
-      children[val] = ID3(examples, mode(sub_e))
-      attributes.remove(best_attr)
-
-    tree.children = children
+      for val, sub_e in sub_examples.iteritems():
+        if best_attr in attributes:
+          attributes.remove(best_attr)
+        children[val] = ID3(examples, mode(sub_e))
+      tree.children = children
   return tree
 
 def prune(node, examples):
@@ -64,35 +79,33 @@ def prune(node, examples):
   q = []  # queue of nodes @ each level
   output = {}
   q.append(node)
-  original_node = node
-  while len(q) != 0:
+  while (len(q) != 0):
     n = q.pop(0) #first node
-    if (n != None && n.label != None): # if we have nodes & haven't reached a leaf node yet
+    #if (n != None and n.label != None): # if we have nodes & haven't reached a leaf node yet
       
-      # save the current node's state before deleting it
-      old_acc = test(n, examples)
-      old_node = n
-      old_children = n.children
+    # save the current node's state before deleting it
+    old_acc = test(n, examples)
+    old_node = n
+    old_children = n.children
       
-      # proceed to delete it
-      n.label = n.mode
-      n.children = {}
-      new_acc = test(n, examples)
+    # proceed to delete it
+    n.label = n.mode
+    n.children = {}
+    new_acc = test(n, examples)
       
-      # if decide to delete it
-      if new_acc >= old_acc:
-        numDeletedNode += 1
-        continue
+    # if decide to delete it
+    if new_acc >= old_acc:
+      numDeletedNode += 1
+      continue
 
-      # if decide to keep it
-      else:
-        #add children
-        if old_children != None:
-          for c in old_children.itervalues():
-            q.append(c)
+    # if decide to keep it
+    else:
+      #add children
+      if old_children != None:
+        for c in old_children.itervalues():
+          q.append(c)
 
-    print int(numDeletedNode)
-    return original_node
+    return node
 
 def test(node, examples):
   '''
@@ -110,7 +123,10 @@ def test(node, examples):
     if computed_class_val == actual_class_val:
       accurate_ex += 1
 
-  accuracy = float(accurate_ex / total_ex)
+  accuracy = accurate_ex / float(total_ex)
+  print total_ex
+  print accurate_ex
+  print accuracy
   return accuracy
 
 def evaluate(node, example):
@@ -118,11 +134,14 @@ def evaluate(node, example):
   Takes in a tree and one example.  Returns the Class value that the tree
   assigns to the example.
   '''
+  nodeVisited = 0
   while len(node.children)!=0: # while you havent reached a leaf
     best_attr = node.split_attr # find which attribute you split on
     ex_split_val = example[best_attr] # find what value the example had for this attribute
     node = node.children[ex_split_val] # go to the child node whose value matches
+    nodeVisited += 1
 
+  print "number of node visited:", nodeVisited
   return node.label
 
 def entropy(examples, target_attr):
@@ -172,6 +191,7 @@ def choose_best_attr(examples, attributes):
   '''
   Choose which attribute to split on (attr w/ max info_gain)
   '''
+  best_attr = None
   max_info_gain = 0
   curr_info_gain = 0
   for a in attributes:
@@ -186,11 +206,32 @@ def check_same_class(examples):
   Check if all examples have the same class value
   Return None(if don't have same class value) or value of the attribute
   '''
-  first_ex_value = examples[0][0]
-  for e in examples[1:]:
-    if e[0] != first_ex_value:
+  first_ex_value = examples[0]['Class']
+  for e in examples:
+    if e['Class'] != first_ex_value:
       return None
   return first_ex_value
+
+def check_all_attr_same_value(examples, attributes):
+  '''
+  Check if all examples have the same attribute value for each attribute
+  Return False or True
+  '''
+  for a in attributes:
+    if check_same_value(examples, a) != True:
+      return False
+  return True
+
+def check_same_value(examples, attr):
+  '''
+  Check if all examples have the same attribute value for each attribute
+  Return False or True
+  '''
+  checking_value = examples[0][attr]
+  for e in examples:
+    if e[attr] != checking_value:
+      return False
+  return True
 
 def target_attr_mode(examples, target_attr):
   '''
@@ -209,7 +250,7 @@ def mode(examples):
   # initialize dictionary
   count = {}
   for e in examples:
-    if e[label] in count:
+    if e['Class'] in count:
       count[e['Class']] += 1.0
     else:
       count[e['Class']] = 1.0
---
+++ test.py
@@ -0,0 +1,6 @@
+import ID3
+import unit_tests as ut
+ut.testID3AndEvaluate()
+#ut.testPruning()
+ut.testID3AndTest()
+# ut.testPruningOnHouseData("house_votes_84.data")
